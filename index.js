06ec299b2834306c0363645b938192529b97063a
Nazar Yaremii <n.yaremiy791@gmail.com>
2023-03-04 13:15:26 +0200

    Added an animation of the button to hide the sidebar

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log('App started!!!');
 
+let isOpenSideBar = false;
+
 const menuItems = document.querySelectorAll('.item');
 
 menuItems.forEach((item) => {
@@ -9,6 +11,7 @@ menuItems.forEach((item) => {
     });
     item.classList.add('active-menu');
     removeClassesForSidebar();
+    rotateBtnSideBarSwitch(isOpenSideBar);
   });
 });
 
@@ -21,33 +24,48 @@ btnSidebar.addEventListener('click', () => {
   document.querySelectorAll('.item__text').forEach((item) => {
     item.classList.toggle('item__text--close');
   });
+  isOpenSideBar = !isOpenSideBar;
+  rotateBtnSideBarSwitch(isOpenSideBar);
 });
 
-function addClassesForSidebar () {
-  btnSidebar.style = 'transform: rotate(180deg)';
+function addClassesForSidebar() {
+  rotateBtnSideBarSwitch(true);
   document.querySelector('.logo__text').classList.add('logo__text--close');
   document.querySelector('.sidebar').classList.add('sidebar--close');
   document.querySelector('.user__info').classList.add('user__info--close');
   document.querySelectorAll('.item__text').forEach((item) => {
     item.classList.add('item__text--close');
   });
+  isOpenSideBar = true;
+  rotateBtnSideBarSwitch(isOpenSideBar);
 }
-function removeClassesForSidebar () {
-  btnSidebar.style = 'transform: rotate(0deg)';
+function removeClassesForSidebar() {
+  rotateBtnSideBarSwitch(false);
   document.querySelector('.logo__text').classList.remove('logo__text--close');
   document.querySelector('.sidebar').classList.remove('sidebar--close');
   document.querySelector('.user__info').classList.remove('user__info--close');
   document.querySelectorAll('.item__text').forEach((item) => {
     item.classList.remove('item__text--close');
   });
+  isOpenSideBar = false;
+  rotateBtnSideBarSwitch(isOpenSideBar);
 }
 
-
-function closeSidebar () {
+function closeSidebar() {
   const widthScreen = window.innerWidth;
-  
-  if(widthScreen <= 768) {
+
+  if (widthScreen <= 768) {
     addClassesForSidebar();
   }
 }
 closeSidebar();
+
+function rotateBtnSideBarSwitch(isOpenSideBar) {
+  if (isOpenSideBar) {
+    btnSidebar.style =
+      'transform: rotate(0deg); transition: transform .3s; transform-origin: center;';
+  } else {
+    btnSidebar.style =
+      'transform: rotate(-180deg); transition: transform .3s; transform-origin: center;';
+  }
+}
